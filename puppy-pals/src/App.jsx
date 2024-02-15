c7e5bea0fc63b90df19afb7ade29dd5506af3474
Vans <53773587+iamfernweh@users.noreply.github.com>
2024-02-15 15:59:48 -0500

    added more lines to app

--- puppy-pals/src/App.jsx
+++ puppy-pals/src/App.jsx
@@ -1,6 +1,11 @@
+import { useState } from "react";
 import './App.css';
+import { puppyList } from "./data.js";
 
 function App() {
+  const [puppies, setPuppies] = useState(puppyList)
+  console.log(puppies);
+  
   return (
     <div>
       {/* You can place any content you want to keep within this div */}
