06db2ca8ec85b6a6ed4cadddfe463619586eb420
Vans <53773587+iamfernweh@users.noreply.github.com>
2024-02-15 16:40:35 -0500

    step 4

--- puppy-pals/src/App.jsx
+++ puppy-pals/src/App.jsx
@@ -4,16 +4,34 @@ import { puppyList } from "./data.js";
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList)
-  
+  const [featPupId, setFeatPupId] = useState(null)
+
   console.log('puppyLIst: ', puppyList);
   
+  function handleClick(puppyId){
+    console.log('puppy id: ', puppyId);
+    setFeatPupId(puppyId);
+  }
+
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+  console.log(featuredPup);
+
   return (
     <div className="App">
-      {
-        puppies.map((puppy) => {
-          return <p key={puppy.id}>{puppy.name}</p>
-        })
-      }
+      {puppies.map((puppy) => {
+          return (
+            <p onClick={() => handleClick(puppy.id)} key={puppy.id}>{puppy.name}</p>
+          );
+        })}
+        {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
